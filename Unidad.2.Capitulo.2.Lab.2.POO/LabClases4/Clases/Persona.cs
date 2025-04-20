e2526c73c6ba36e3b521f5c0333c36009c6e251b
Franco Zariaga <francozariaga.zariaga@gmail.com>
2025-04-20 20:28:41 -0300

    Lab 4 Unidad 2 Capitulo 2
    Clase Persona

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases4/Clases/Persona.cs
@@ -0,0 +1,53 @@
+﻿namespace Clases
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private string dni;
+        private int edad;
+
+        public Persona(string nombre, string apellido, int edad, string dni)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+            this.edad = edad;
+            Console.WriteLine("Persona creada correctamente");
+        }
+        ~Persona()
+        {
+            Console.WriteLine("Persona destruida correctamente");
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return Apellido; }
+            set { Apellido = value; }
+        }
+        public string Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+        public string Edad
+        {
+            get { return edad.ToString(); }
+            set { edad = int.Parse(value); }
+        }
+
+        public string getFullName()
+        {
+            return $"{nombre} {apellido}";
+        }
+        public string getAge()
+        {
+            return $"{edad}";
+        }
+    }
+}
---
+++ Unidad.2.Capitulo.2.Lab.2.POO/LabClases4/PruebaPersona/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Clases;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona("Juan","Tinturelli", 30, "45215900");
+
+        Console.WriteLine($"Nombre completo: {persona.getFullName()}" );
+        Console.WriteLine($"Edad: {persona.getAge()}");
+        Console.WriteLine($"DNI: {persona.Dni}");
+
+        Console.ReadKey();
+    }
+}
+
+
